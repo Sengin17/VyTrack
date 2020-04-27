0b6eeac50a223ee5ef9ab2793e6b2087f269d695
sengin <enginsema@gmail.com>
2020-04-27 14:40:36 -0600

    first commit

---
+++ src/test/java/com/automation/pages/activities/CalendarEventsPage.java
@@ -0,0 +1,262 @@
+package com.automation.pages.activities;
+
+import com.automation.pages.AbstractPageBase;
+import com.automation.utilities.BrowserUtilities;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+
+public class CalendarEventsPage extends AbstractPageBase {
+    public WebDriver driver = Driver.getDriver();
+
+    @FindBy(css = "[title='Create Calendar event']")
+    private WebElement createCalendarEvent;
+
+    @FindBy(className = "select2-chosen")
+    private WebElement owner;
+
+    @FindBy(css = "[id^='date_selector_oro_calendar_event_form_start']")
+    public WebElement startDate;
+
+    @FindBy(css = "[id^='time_selector_oro_calendar_event_form_start']")
+    public WebElement startTime;
+
+    @FindBy(css = "[id^='time_selector_oro_calendar_event_form_end']")
+    public WebElement endTime;
+
+    @FindBy(className = "grid-header-cell__label")
+    private List<WebElement> columnNames;
+
+    @FindBy(css = "iframe[id^='oro_calendar_event_form_description-uid']")
+    private WebElement descriptionFrame;
+
+    @FindBy(css = "[id^='oro_calendar_event_form_title-uid']")
+    private WebElement title;
+
+    @FindBy(id = "tinymce")
+    private WebElement descriptionTextArea;
+
+    @FindBy(css = "[class='btn-group pull-right'] > button")
+    private WebElement saveAndClose;
+
+    @FindBy(xpath = "(//div[@class='control-label'])[1]")
+    private WebElement generalInfoTitle;
+
+    @FindBy(xpath = "//label[text()='Description']/following-sibling::div//div")
+    private WebElement generalInfoDescription;
+
+
+    public void enterCalendarEventTitle(String titleValue) {
+        BrowserUtilities.waitForPageToLoad(22);
+        wait.until(ExpectedConditions.visibilityOf(title)).sendKeys(titleValue);
+    }
+
+    public void enterCalendarEventDescription(String description) {
+        //wait until frame is available and switch to it
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(descriptionFrame));
+        descriptionTextArea.sendKeys(description);
+        driver.switchTo().defaultContent();//exit from the frame
+    }
+
+    public void clickOnSaveAndClose() {
+        wait.until(ExpectedConditions.elementToBeClickable(saveAndClose)).click();
+    }
+
+    public String getGeneralInfoTitleText() {
+        BrowserUtilities.waitForPageToLoad(22);
+        return generalInfoTitle.getText();
+    }
+
+    public String getGeneralInfoDescriptionText() {
+        BrowserUtilities.waitForPageToLoad(22);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//label[text()='Description']/following-sibling::div//div")));
+        return generalInfoDescription.getText();
+    }
+
+
+    //#############################################################
+    public List<String> getColumnNames() {
+        BrowserUtilities.waitForPageToLoad(20);
+        return BrowserUtilities.getTextFromWebElements(columnNames);
+    }
+
+    public String getStartTime() {
+        BrowserUtilities.waitForPageToLoad(20);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[id^='time_selector_oro_calendar_event_form_start']")));
+        wait.until(ExpectedConditions.visibilityOf(startTime));
+        return startTime.getAttribute("value");
+    }
+
+    public String getEndTime() {
+        BrowserUtilities.waitForPageToLoad(20);
+        wait.until(ExpectedConditions.visibilityOf(endTime));
+        return endTime.getAttribute("value");
+    }
+
+    public String getOwnerName() {
+        BrowserUtilities.waitForPageToLoad(20);
+        //wait for element to be present in DOM
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("select2-chosen")));
+        wait.until(ExpectedConditions.visibilityOf(owner));
+        return owner.getText().trim();
+    }
+
+    public void clickToCreateCalendarEvent() {
+        BrowserUtilities.waitForPageToLoad(20);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[title='Create Calendar event']")));
+        wait.until(ExpectedConditions.elementToBeClickable(createCalendarEvent)).click();
+        BrowserUtilities.waitForPageToLoad(20);
+    }
+
+    public String getStartDate() {
+        BrowserUtilities.waitForPageToLoad(20);
+        wait.until(ExpectedConditions.visibilityOf(startDate));
+        BrowserUtilities.scrollTo(startDate);
+        return startDate.getAttribute("value");
+    }
+
+    // -------------------HW 5-----------------------------
+
+    @FindBy(xpath ="//*[@href='/calendar/event/view/1846']")
+    public WebElement view;
+    @FindBy(xpath = "//*[@href='/calendar/event/update/1846']")
+    public WebElement edit;
+    @FindBy(xpath = "//*[@href='#']")
+    public WebElement delete;
+
+    @FindBy(xpath = "//td[text()='Testers Meeting']//following-sibling::td//a[text()='...']")
+    public WebElement  treeDots;
+
+    @FindBy(css = "[class='fa-cog hide-text']")
+    public WebElement gridOptionBtn;
+
+    @FindBy(xpath = "//span[text()='Title']")
+    public WebElement titleColumn;
+
+    @FindBy(css = "[class='btn-success btn dropdown-toggle']")
+    public WebElement saveAndCloseDropDown;
+
+    @FindBy(xpath = "//ul//li//button[contains(text(),'Save and Close')]")
+    public WebElement saveAndCloseOption;
+
+    @FindBy(xpath = "//ul//li//button[contains(text(),'Save and New')]")
+    public WebElement saveAndNewsOption;
+
+    @FindBy(xpath = "(//ul//li//button[contains(text(),Save)])[3]")
+    public WebElement saveOption;
+
+    @FindBy(xpath = "//*[@title='Cancel']")
+    public WebElement cancelBtn;
+
+    @FindBy(className = "oro-subtitle")
+    public WebElement allCalendarEvents;
+
+    @FindBy(xpath = "//li[text()='9:00 PM']")
+    public WebElement time9Pm;
+
+    @FindBy (css ="[id^='oro_calendar_event_form_allDay']")
+    public WebElement allDayCheckBox;
+
+    @FindBy(css = "[id^='date_selector_oro_calendar_event_form_end']")
+    public WebElement endDate;
+
+    @FindBy(css ="[id^='recurrence-repeat-view']")
+    public WebElement repeatCheckBox;
+
+    @FindBy(css ="[id^='recurrence-repeats-view']")
+    public WebElement repeatsDropBox;
+
+    @FindBy(xpath = "//label[text()='Summary:']")
+    public WebElement summary;
+
+    @FindBy(xpath = "//div[@data-name='recurrence-summary']//div/span[1]")
+    public WebElement summary1;
+
+    @FindBy ( xpath= "//div[@data-name='recurrence-summary']//div/span[2]")
+    public WebElement summary2;
+
+    @FindBy(xpath = "//input[@checked='checked']")
+    public WebElement repeatEveryDays;
+
+    @FindBy(xpath = "(//input[@type='radio'])[3]")
+    public WebElement endsNever;
+
+    @FindBy(xpath = "(//input[@type='radio'])[4]")
+    public WebElement endsAfter;
+
+    @FindBy(xpath = "(//*[@class=\"recurrence-subview-control__number\"])[7]")
+    public WebElement afterOccurrences;
+
+    @FindBy(xpath = "(//input[@type=\"radio\"])[5]")
+    public WebElement byDateCheckBox;
+
+    @FindBy(xpath = "//*[@class='datepicker-input hasDatepicker']")
+    public WebElement dateTextBox;
+
+    @FindBy(className = "ui-datepicker-month")
+    public WebElement datePickerMonth;
+
+    @FindBy(xpath = "//div//span[text()='Daily every 1 day']/following-sibling::span")
+    public WebElement byDatePickerOccurrence;
+
+    @FindBy(className = "ui-datepicker-year")
+    public WebElement datePickerYear;
+
+    @FindBy(css ="[id^='recurrence-repeats-view']")
+    public WebElement repeatsSelector;
+
+    public WebElement weekDayCheckBox(String firstLetterOfDay){
+        return Driver.getDriver().findElement(By.xpath("//span[text()='"+firstLetterOfDay+"']"));
+    }
+
+    public WebElement datePickerDay(String dayNum){
+        String xpath = "//a[text()='"+dayNum+"']";
+        return Driver.getDriver().findElement(By.xpath(xpath));
+    }
+
+    public void clickCancelBtn(){
+
+        wait.until(ExpectedConditions.elementToBeClickable(cancelBtn)).click();
+
+    }
+    public void clickSaveAndCloseDropDown(){
+
+        wait.until(ExpectedConditions.elementToBeClickable(saveAndCloseDropDown)).click();
+    }
+
+    public void clickOnGridOptionBtn(){
+
+        wait.until(ExpectedConditions.elementToBeClickable(gridOptionBtn)).click();
+
+    }
+
+    public void getTreeDots(){
+
+        Actions actions = new Actions(driver);
+        BrowserUtilities.wait(3);
+        actions.moveToElement(treeDots).pause(2000).perform();
+
+    }
+    public Integer timeDifference(){
+        String startTime = getStartTime();
+        String endTime =getEndTime();
+
+        String [] start =startTime.split(":");
+        int startValue = Integer.parseInt(start[0]);
+
+        String [] end =endTime.split(":");
+        int endValue = Integer.parseInt(end[0]);
+        if (startValue==12){
+            startValue-=12;
+        }
+
+        return endValue-startValue;
+    }
+
+}
---
+++ src/test/java/com/automation/tests/activities/CalendarEventsTestPage.java
@@ -0,0 +1,383 @@
+package com.automation.tests.activities;
+
+import com.automation.pages.LoginPage;
+import com.automation.pages.activities.CalendarEventsPage;
+import com.automation.tests.AbstractTestBase;
+import com.automation.utilities.BrowserUtilities;
+import com.automation.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class CalendarEventsTestPage extends AbstractTestBase {
+
+    public WebDriver driver = Driver.getDriver();
+    LoginPage loginPage = new LoginPage();
+
+
+    /**
+     * Test Case #1
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Hover on three dots “…” for “Testers meeting”
+     * calendar event
+     * 5. Verify that “view”, “edit” and “delete” options
+     * are available
+     */
+
+    @Test
+    public void test1() {
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that “view”, “edit” and “delete” options");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.getTreeDots();
+        Assert.assertTrue(calendarEventsPage.view.isDisplayed());
+        Assert.assertTrue(calendarEventsPage.edit.isDisplayed());
+        Assert.assertTrue(calendarEventsPage.delete.isDisplayed());
+        test.pass("View, Edit and Delete options verified");
+    }
+
+    /**
+     * Test Case #2 1.Go to “https://qa1.vytrack.com/"
+     * 2.Login as a store manager
+     * 3.Navigate to “Activities -> Calendar Events”
+     * 4.Click on “Grid Options” button
+     * 5.Deselect all options except “Title”
+     * 6.Verify that “Title” column still displayed
+     */
+    @Test
+    public void test2() {
+
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that “Title” column still displayed");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickOnGridOptionBtn();
+
+
+        List<WebElement> gridOptions = driver.findElements(By.xpath("//input[@data-role='renderable']"));
+        for (int i = 1; i < gridOptions.size(); i++) {
+            gridOptions.get(i).click();
+            BrowserUtilities.wait(1);
+        }
+        System.out.println(gridOptions.size());
+        BrowserUtilities.wait(3);
+        Assert.assertTrue(calendarEventsPage.titleColumn.isDisplayed());
+        test.pass("Title column verified");
+
+    }
+
+    /**
+     * Test Case #3
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Expand “Save And Close” menu
+     * 6. Verify that “Save And Close”, “Save And New”
+     * and “Save” options are available
+     */
+    @Test
+    public void test3() {
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that “Save And Close”, “ Save And New ” and “ Save ” ");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        calendarEventsPage.clickSaveAndCloseDropDown();
+
+        Assert.assertEquals(calendarEventsPage.saveAndCloseOption.getText().trim(), "Save And Close");
+        Assert.assertEquals(calendarEventsPage.saveAndNewsOption.getText().trim(), "Save And New");
+        Assert.assertEquals(calendarEventsPage.saveOption.getText().trim(), "Save");
+
+        test.pass("“Save And Close”, “Save And New  and “Save” verified");
+
+    }
+
+    /**
+     * Test Case #4
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Then, click on “Cancel” button
+     * 6. Verify that “All Calendar Events” page subtitle is
+     * displayed
+     */
+    @Test
+    public void test4() {
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that “All Calendar Events” page subtitle is displayed");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        calendarEventsPage.clickCancelBtn();
+        BrowserUtilities.wait(3);
+        Assert.assertTrue(calendarEventsPage.allCalendarEvents.isDisplayed());
+
+        test.pass("“All Calendar Events” page subtitle is displayed");
+
+    }
+
+    /**
+     * Test Case #5
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Verify that difference between end and start time
+     * is exactly 1 hour
+     */
+    @Test
+    public void test5() {
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that difference between end and start time is exactly 1 hour");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        Assert.assertTrue(calendarEventsPage.timeDifference() == 1);
+
+        test.pass("Time differences verified");
+    }
+
+    /**
+     * Test Case #6
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Select “9:00 PM” as a start time
+     * 6. Verify that end time is equals to “10:00 PM”
+     */
+    @Test
+    public void test6() {
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that end time is equals to “10:00 PM”");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        calendarEventsPage.getStartTime();
+        Actions actions = new Actions(driver);
+        BrowserUtilities.wait(4);
+        actions.moveToElement(calendarEventsPage.time9Pm).click().perform();
+        BrowserUtilities.wait(2);
+        Assert.assertEquals(calendarEventsPage.getEndTime(), "10:00 PM");
+
+    }
+
+    /**
+     * Test Case #7
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Select “All-Day Event” checkbox
+     * 6. Verify that “All-Day Event” checkbox is selected
+     * 7. Verify that start and end time input boxes are
+     * not displayed
+     * 8. Verify that start and end date input boxes are
+     * displayed
+     */
+    @Test
+    public void test7() {
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that start and end date input boxes are displayed");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        BrowserUtilities.wait(4);
+        calendarEventsPage.allDayCheckBox.click();
+        BrowserUtilities.wait(3);
+        Assert.assertTrue(calendarEventsPage.allDayCheckBox.isSelected());
+        BrowserUtilities.wait(3);
+        Assert.assertTrue(calendarEventsPage.startTime.isEnabled());
+        Assert.assertTrue(calendarEventsPage.endTime.isEnabled());
+        Assert.assertTrue(calendarEventsPage.startDate.isDisplayed());
+        Assert.assertTrue(calendarEventsPage.endDate.isDisplayed());
+    }
+
+    /**
+     * Test Case #8
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Select “Repeat” checkbox
+     * 6. Verify that “Repeat” checkbox is selected
+     * 7. Verify that “Daily” is selected by default and
+     * following options are available in
+     * “Repeats” drop-down:
+     */
+    @Test
+    public void test8() {
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that “Daily” is selected by default and following options are available in “Repeats” drop-down:");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        BrowserUtilities.wait(4);
+        calendarEventsPage.repeatCheckBox.click();
+
+        Assert.assertTrue(calendarEventsPage.repeatCheckBox.isSelected());
+
+        Select select = new Select(calendarEventsPage.repeatsDropBox);
+        Assert.assertEquals(select.getFirstSelectedOption().getText(), "Daily");
+        List<WebElement> repeatsSelect = select.getOptions();
+
+        Assert.assertEquals(repeatsSelect.get(0).getText(), "Daily");
+        Assert.assertEquals(repeatsSelect.get(1).getText(), "Weekly");
+        Assert.assertEquals(repeatsSelect.get(2).getText(), "Monthly");
+        Assert.assertEquals(repeatsSelect.get(3).getText(), "Yearly");
+    }
+
+    /**
+     * Test Case #9
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Select “Repeat” checkbox
+     * 6. Verify that “Repeat” checkbox is selected
+     * 7. Verify that “Repeat Every” radio button is
+     * selected
+     * 8. Verify that “Never” radio button is selected as an
+     * “Ends” option.
+     * 9. Verify that following message as a summary is
+     * displayed: “Summary: Daily every 1 day”
+     */
+    @Test
+    public void test9() {
+
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that “Never” radio button is selected as an “Ends” option");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        BrowserUtilities.wait(4);
+        calendarEventsPage.repeatCheckBox.click();
+
+        Assert.assertTrue(calendarEventsPage.repeatCheckBox.isSelected());
+        Assert.assertTrue(calendarEventsPage.repeatEveryDays.isSelected());
+        Assert.assertTrue(calendarEventsPage.endsNever.isSelected());
+        String expected ="Summary: Daily every 1 day";
+        String actual = calendarEventsPage.summary.getText()+" "+calendarEventsPage.summary1.getText();
+        Assert.assertEquals(actual,expected);
+    }
+    /**
+     * Test Case #10
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Select “Repeat” checkbox
+     * 6. Select “After 10 occurrences” as an “Ends”
+     * option.
+     * 7. Verify that following message as a summary is
+     * displayed: “Summary: Daily every 1 day, end
+     * after 10 occurrences”
+     */
+    @Test
+    public void test10(){
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that following message as a summary is displayed: “Summary: Daily every 1 day, end after 10 occurrences");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        BrowserUtilities.wait(4);
+        calendarEventsPage.repeatCheckBox.click();
+
+        calendarEventsPage.endsAfter.click();
+        calendarEventsPage.afterOccurrences.sendKeys("10", Keys.ENTER);
+
+        String expected ="Summary: Daily every 1 day, end after 10 occurrences";
+        String actual = calendarEventsPage.summary.getText()+
+                " "+calendarEventsPage.summary1.getText()+
+                calendarEventsPage.summary2.getText();
+        Assert.assertEquals(actual,expected);
+
+        test.pass("Verified");
+    }
+
+    /**
+     * Test Case #11
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Select “Repeat” checkbox
+     * 6. Select “By Nov 18, 2021” as an “Ends” option.
+     * 7. Verify that following message as a summary is
+     * displayed: “Summary: Daily every 1 day, end by
+     * Nov 18, 2021”
+     */
+    @Test
+    public void test11(){
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that following message as a summary is displayed: “Summary: Daily every 1 day, end by Nov 18, 2021");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        BrowserUtilities.wait(4);
+        calendarEventsPage.repeatCheckBox.click();
+        calendarEventsPage.byDateCheckBox.click();
+        calendarEventsPage.dateTextBox.click();
+        Select selectMonth = new Select(calendarEventsPage.datePickerMonth);
+        selectMonth.selectByVisibleText("Nov");
+        Select selectYear = new Select(calendarEventsPage.datePickerYear);
+        selectYear.selectByVisibleText("2021");
+        calendarEventsPage.datePickerDay("18").click();
+
+        String expected ="Summary: Daily every 1 day, end by Nov 18, 2021";
+
+        String actual = calendarEventsPage.summary.getText() +  " " +
+                        calendarEventsPage.summary1.getText() +
+                        calendarEventsPage.byDatePickerOccurrence.getText();
+
+        Assert.assertEquals(actual,expected);
+
+    }
+
+    /**
+     * Test Case #12
+     * 1. Go to “https://qa1.vytrack.com/"
+     * 2. Login as a store manager
+     * 3. Navigate to “Activities -> Calendar Events”
+     * 4. Click on “Create Calendar Event” button
+     * 5. Select “Repeat” checkbox
+     * 6. Select “Weekly” options as a “Repeat” option
+     * 7. Select “Monday and Friday” options as a
+     * “Repeat On” options
+     * 8. Verify that “Monday and Friday” options are
+     * selected
+     * 9. Verify that following message as a summary is
+     * displayed: “Summary: Weekly every 1 week on
+     * Monday, Friday”
+     */
+    @Test
+    public void test12(){
+        loginPage.login();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        test = report.createTest("Verify that following message as a summary is displayed: “Summary: Weekly every 1 week on Monday, Friday");
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        BrowserUtilities.wait(4);
+        calendarEventsPage.repeatCheckBox.click();
+        Select select = new Select(calendarEventsPage.repeatsSelector);
+        select.selectByVisibleText("Weekly");
+        calendarEventsPage.weekDayCheckBox("M").click();
+        calendarEventsPage.weekDayCheckBox("F").click();
+        String expected = "Summary: Weekly every 1 week on Monday, Friday";
+        String actual = calendarEventsPage.summary.getText() + " " +
+                        calendarEventsPage.summary1.getText();
+        Assert.assertEquals(actual,expected);
+    }
+}
